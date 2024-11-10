8aed433f43f2b29be45c31db793830d468bc7b99
HSCloud <hscloud913@gmail.com>
2024-11-10 17:17:30 +0900

    Fix : 디자인 패턴, google test 추가
    - Singleton, Observer, Command 패턴 추가

---
+++ Base/Test/Interface/test_command.cpp
@@ -0,0 +1,81 @@
+//
+// Created by hsclo on 24. 11. 10.
+//
+
+#include <gtest/gtest.h>
+#include "Interface/ICommand.h"
+
+
+
+class TestCommand final : public ne::ICommand
+{
+public:
+	virtual void Execute() override
+	{
+		isExecuted = true;
+	}
+
+	virtual void Undo() override
+	{
+		isExecuted = false;
+	}
+
+	[[nodiscard]] bool IsExecuted() const
+	{
+		return isExecuted;
+	}
+
+private:
+	bool isExecuted = false;
+};
+
+class CommandTest : public ::testing::Test
+{
+protected:
+	std::unique_ptr<ne::IInvoker> invoker;
+	std::shared_ptr<TestCommand> command;
+
+	void SetUp() override
+	{
+		invoker = std::make_unique<ne::IInvoker>();
+		command = std::make_shared<TestCommand>();
+	}
+};
+
+
+
+TEST_F(CommandTest, ExecuteCommand)
+{
+	invoker->Push(command);
+	invoker->Execute();
+	EXPECT_TRUE(command->IsExecuted());
+}
+
+TEST_F(CommandTest, UndoCommand)
+{
+	invoker->Push(command);
+	invoker->Execute();
+	invoker->Undo();
+	EXPECT_FALSE(command->IsExecuted());
+}
+
+TEST_F(CommandTest, RedoCommand)
+{
+	invoker->Push(command);
+	invoker->Execute();
+	invoker->Undo();
+	invoker->Redo();
+	EXPECT_TRUE(command->IsExecuted());
+}
+
+TEST_F(CommandTest, UndoEmptyHistory)
+{
+	invoker->Undo();
+	EXPECT_FALSE(command->IsExecuted());
+}
+
+TEST_F(CommandTest, RedoEmptyHistory)
+{
+	invoker->Redo();
+	EXPECT_FALSE(command->IsExecuted());
+}
